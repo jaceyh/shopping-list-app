c72cd5dbd5205c0665ecfaecbdb99546493d8aca
jaceyh <jacey.hallock@gmail.com>
2023-12-30 10:38:50 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,13 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, LogBox, Alert } from 'react-native';
+import { useEffect } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+//import firestore
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+//import useNetInfo to determine user's connectivity status
+import { useNetInfo } from "@react-native-community/netinfo";
+
+// import the screens
+import ShoppingLists from './components/ShoppingLists';
+import Welcome from './components/Welcome';
+import { ScreenStackHeaderLeftView } from 'react-native-screens';
+
+const App = () => {
+
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyBh0zuVFwMcRUra8QTQYhs9YnZgzHvyHKE",
+        authDomain: "shopping-list-demo-55e58.firebaseapp.com",
+        projectId: "shopping-list-demo-55e58",
+        storageBucket: "shopping-list-demo-55e58.appspot.com",
+        messagingSenderId: "14472864734",
+        appId: "1:14472864734:web:e7fbb8c794ce0e9f9db34f"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    // Initialize Cloud Firestore and get a reference to the service
+    const db = getFirestore(app);
+
+    //define a new state the represents connectivity status
+    const connectionStatus = useNetInfo();
+
+    //display an alert if connection is lost
+    useEffect(() => {
+        if (connectionStatus.isConnected === false) Alert.alert("Connection lost!")
+    }, [connectionStatus.isConnected]);
+    
+
+
+    return (
+        <NavigationContainer style={styles.container}>
+            <Stack.Navigator
+            initialRouteName="Welcome"
+            >
+                <Stack.Screen 
+                name="Welcome" 
+                component={Welcome} 
+                />
+                <Stack.Screen
+                name='ShoppingLists'
+                >
+                {props => <ShoppingLists db={db} {...props} />}
+                </Stack.Screen>
+            </Stack.Navigator>
+            <StatusBar style="auto" />
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +77,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
